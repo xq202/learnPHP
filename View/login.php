26916cf56f331764521a1341a65ff9ce2857a633
xq202 <Xq01022002@gmail.com>
2023-07-08 08:08:51 +0700

    modified:   View/login.php

--- View/login.php
+++ View/login.php
@@ -2,6 +2,8 @@
 session_start(); 
 if(!isset($_SESSION["username_error"])) $_SESSION["username_error"] = 0;
 if(!isset($_SESSION["password_error"])) $_SESSION["password_error"] = 0;
+if(!isset($_SESSION["username"])) $_SESSION["username"]="";
+if(!isset($_SESSION["password"])) $_SESSION["password"]="";
 // echo $_SESSION["username_error"].'<br>'.$_SESSION["password_error"];
 ?>
 <!DOCTYPE html>
@@ -24,15 +26,21 @@ if(!isset($_SESSION["password_error"])) $_SESSION["password_error"] = 0;
         <div class="login_content">
             <div class="login_form">
                 <form action="login.php" id="login_form" method="post">
-                    <input type="text" placeholder="Tài khoản" name="username">
+                    <input type="text" placeholder="Tài khoản" name="username" value="<?= $_SESSION["username"] ?>">
                     <?php
-                    if($_SESSION["username_error"]==1){
+                    if(trim($_SESSION["username"])==""){
+                        echo "Hãy nhập tài khoản<br>";
+                    }
+                    elseif($_SESSION["username_error"]==1){
                         echo "Tài khoản không tồn tại<br>";
                     }
                     ?>
                     <input type="password" placeholder="Mật khẩu" name="password">
                     <?php
-                    if($_SESSION["password_error"]==1){
+                    if(trim($_SESSION["password"])=="" && $_SESSION["username_error"]==0){
+                        echo "Hãy nhập mật khẩu<br>";
+                    }
+                    elseif($_SESSION["password_error"]==1){
                         echo "Mật khẩu sai<br>";
                     }
                     ?>
@@ -53,6 +61,8 @@ if(!isset($_SESSION["password_error"])) $_SESSION["password_error"] = 0;
 // echo base64_encode($iv);
 include("../Controller/LoginController.php");
 if(isset($_POST["login"])){
+    $_SESSION["username"] = $_POST["username"];
+    $_SESSION["password"] = $_POST["password"];
     $check = login($_POST["username"], $_POST["password"]);
     if($check==1){
         $_SESSION["account"] = base64_encode($_POST["username"]);
@@ -61,12 +71,14 @@ if(isset($_POST["login"])){
     elseif ($check==0) {
         $_SESSION["username_error"]=1;
         $_SESSION["password_error"]=0;
-        header("location: login.php");
+        header("Refresh:0");
+        exit();
     }
     else{
         $_SESSION["username_error"]=0;
         $_SESSION["password_error"]=1;
-        header("location: login.php");
+        header("Refresh:0");
+        exit();
     }
 }
 ?>
