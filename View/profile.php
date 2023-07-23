3621ab1a2d4bb93a22727934e3e19ff3ae22d6cf
xq202 <xq01022002@gmail.com>
2023-07-23 09:01:30 +0700

    new file:   Data/Photo/avatar/avatar-mac-dinh.jpg 	new file:   Data/Photo/cover_photo/no-cover-photo.jpg 	modified:   View/css/profile.css 	modified:   View/profile.php

--- View/profile.php
+++ View/profile.php
@@ -8,31 +8,36 @@
     <link rel="stylesheet" href="./View/css/profile.css">
 </head>
 <body>
-    <div class="phan1">
-        <div class="anh-bia"><img src="./Data/Photo/anhbia.png" alt="anh bia"></div>
-        <div class="thong-tin-co-ban">
-            <div class="anh-dai-dien"><img src="./Data/Photo/anh_dai_dien.png" alt="anh dai dien"></div>
-            <div class="ben-phai-anh-dai-dien">
-                <div class="ten-va-so-ban-be">
-                    <h1>name</h1>
-                    <p>x ban be</p>
-                    <p>x ban chung</p>
-                </div>
-                <div class="tuy-chon">
-                    <button class="bt-tuy-chon">ban be</button>
-                    <button class="bt-nhan-tin">nhan tin</button>
+    <div class="background">
+        <div class="phan1">
+            <div class="anh-bia"><img src="./Data/Photo/cover_photo/no-cover-photo.jpg" alt="anh bia"></div>
+            <div class="thong-tin-co-ban">
+                <div class="anh-dai-dien"><img src="./Data/Photo/avatar/avatar-mac-dinh.jpg" alt="anh dai dien"></div>
+                <div class="ben-phai-anh-dai-dien">
+                    <div class="ten-va-so-ban-be">
+                        <h1>name</h1>
+                        <p>x ban be</p>
+                        <p>x ban chung</p>
+                    </div>
+                    <div class="tuy-chon">
+                        <button class="bt-tuy-chon">ban be</button>
+                        <button class="bt-nhan-tin">nhan tin</button>
+                    </div>
                 </div>
             </div>
+            <div class="menu-trang-ca-nhan">
+                    <ul>
+                        <li>bai viet</li>
+                        <li>gioi thieu</li>
+                        <li>ban be</li>
+                        <li>anh</li>
+                        <li>video</li>
+                    </ul>
+                </div>
         </div>
-        <div class="menu-trang-ca-nhan">
-                <ul>
-                    <li>bai viet</li>
-                    <li>gioi thieu</li>
-                    <li>ban be</li>
-                    <li>anh</li>
-                    <li>video</li>
-                </ul>
-            </div>
+    </div>
+    <div class="phan2">
+        
     </div>
 </body>
 </html>
